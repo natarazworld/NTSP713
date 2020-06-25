c6c0cd7699c305036b0c17673fb90ad603825b69
Admin <Admin@DESKTOP-2VT4A0N>
2020-06-25 13:52:43 +0530

    spring Core

---
+++ IOCProj1-SetterInjecion-POC/src/com/nt/beans/WishMessageGenerator.java
@@ -0,0 +1,31 @@
+package com.nt.beans;
+
+import java.util.Date;
+
+public class WishMessageGenerator {
+	//bean property
+	private  Date  date;
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+   
+	public  String  generateWishMessage(String user) {
+		 int hour=0;
+		//get current hour of the day
+		 hour=date.getHours();
+		 //generate wish message (b.logic)
+		 if(hour<12)
+			 return "Good Morning::"+user;
+		 else if(hour<16)
+			 return "Good Afternoon::"+user;
+		 else if(hour<20)
+			 return "Good Evening ::"+user;
+		 else
+			 return "Good Night ::"+user;
+		 
+		
+	}
+	
+
+}
---
+++ IOCProj1-SetterInjecion-POC/src/com/nt/test/SetterInjectionTest.java
@@ -0,0 +1,32 @@
+package com.nt.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import com.nt.beans.WishMessageGenerator;
+
+public class SetterInjectionTest {
+
+	public static void main(String[] args) {
+		System.out.println("SetterInjectionTest.main()");
+		 Resource res=null;
+		 BeanFactory factory=null;
+		 WishMessageGenerator generator=null;
+		 Object obj=null;
+		 String result=null; 
+		//hold name and location of spring bean cfg file
+		 res=new FileSystemResource("src/com/nt/cfgs/applicationContext.xml");
+		 //create BeanFactory IOC container
+		 factory=new XmlBeanFactory(res);
+		 //get Target bean class object
+		 obj=factory.getBean("wmg");
+		 //typecasting
+		 generator=(WishMessageGenerator)obj;
+		 //invoke the  method
+		 result=generator.generateWishMessage("raja");
+		 System.out.println("Message::"+result);
+		
+	}//main
+}//class
