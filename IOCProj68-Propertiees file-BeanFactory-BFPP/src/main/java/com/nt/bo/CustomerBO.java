f3fbe9941adabb473cdf606d4d480c9be34cc3e5
natarazworld <natarazworld@gmail.com>
2020-09-28 08:41:46 +0530

    BPFP

---
+++ IOCProj68-Propertiees file-BeanFactory-BFPP/src/main/java/com/nt/bo/CustomerBO.java
@@ -0,0 +1,36 @@
+package com.nt.bo;
+
+public class CustomerBO {
+	private String cname;
+	private String cadd;
+	private float pamt;
+	private  float intramt;
+	//getters & setters 
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public String getCadd() {
+		return cadd;
+	}
+	public void setCadd(String cadd) {
+		this.cadd = cadd;
+	}
+	public float getPamt() {
+		return pamt;
+	}
+	public void setPamt(float pamt) {
+		this.pamt = pamt;
+	}
+	public float getIntramt() {
+		return intramt;
+	}
+	public void setIntramt(float intramt) {
+		this.intramt = intramt;
+	}
+	
+	
+
+}
---
+++ IOCProj68-Propertiees file-BeanFactory-BFPP/src/main/java/com/nt/test/RealtimeDITest.java
@@ -0,0 +1,61 @@
+package com.nt.test;
+
+import java.util.Scanner;
+
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.core.env.PropertiesPropertySource;
+
+import com.nt.controller.MainController;
+import com.nt.vo.CustomerVO;
+
+public class RealtimeDITest {
+
+	public static void main(String[] args) {
+		Scanner sc = null;
+		String name = null, cadd = null, pamt = null, rate = null, time = null;
+		CustomerVO vo = null;
+		DefaultListableBeanFactory factory = null;
+		XmlBeanDefinitionReader reader = null;
+		MainController controller = null;
+		String result = null;
+		PropertyPlaceholderConfigurer pphc=null;
+		// read inputs
+		sc = new Scanner(System.in);
+		System.out.println("enter Customername :: ");
+		name = sc.next();
+		System.out.println("Enter customer Addrs::");
+		cadd = sc.next();
+		System.out.println("Enter Principle  amount::");
+		pamt = sc.next();
+		System.out.println("Enter rate of intrest::");
+		rate = sc.next();
+		System.out.println("Enter time   ::");
+		time = sc.next();
+		// Store inputs in VO class object
+		vo = new CustomerVO();
+		vo.setCname(name);
+		vo.setCadd(cadd);
+		vo.setpAmt(pamt);
+		vo.setRate(rate);
+		vo.setTime(time);
+		// create BEanFacory IOC container
+		factory = new DefaultListableBeanFactory();
+		reader = new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions("com/nt/cfgs/applicationContext.xml");
+		//get PropertyPlaceHolderConfigurer
+		pphc=factory.getBean("pphc",PropertyPlaceholderConfigurer.class);
+		pphc.postProcessBeanFactory(factory);
+		// get Controller Bean class object..
+		controller = factory.getBean("controller", MainController.class);
+		// invoke the method
+		try {
+			result = controller.processCustomer(vo);
+			System.out.println(result);
+		} catch (Exception e) {
+			System.out.println("Internal Problem");
+			e.printStackTrace();
+		}
+	}// main
+}// class
